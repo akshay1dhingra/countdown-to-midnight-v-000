40b9de7f50b402422064beba1b15127b339eb29e
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-10-31 01:53:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(n)
   "HAPPY NEW YEAR!"
 end
 
-def countdown(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
     sleep(1)
-    n -= 1
+    seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
 end
